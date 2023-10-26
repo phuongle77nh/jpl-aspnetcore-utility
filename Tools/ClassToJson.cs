3629931fae5e2165a595bb77c4c280113619d5af
phuong <phuongle77nh@gmail.com>
2023-10-26 15:05:50 +0700

    tool generate json

---
+++ Tools/ClassToJson.cs
@@ -0,0 +1,22 @@
+﻿
+// See https://aka.ms/new-console-template for more information
+
+public class HolidayType
+{
+    public string TenantId { get; set; }
+
+    /// <summary>
+    /// Name of leave type.
+    /// </summary>
+    public string? Name { get; set; }
+    public string? Description { get; set; }
+    public DateTime? StartDate { get; set; }
+
+    public DateTime? EndDate { get; set; }
+
+    public bool IsActive { get; set; }
+
+    public HolidayType()
+    {
+    }
+}
---
+++ Tools/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Text.Json;
+
+GenerateHolidayTypes();
+
+static void GenerateHolidayTypes()
+{
+    List<string> types = new List<string>()
+    {
+        "New Year's Day",
+        "Tet Holiday",
+        "Hung Kings Festival",
+        "Reunification Day",
+        "International Labor Day",
+        "Vietnam National Day",
+        "National Day Holiday"
+    };
+
+    var items = new List<HolidayType>();
+    foreach (string type in types)
+    {
+        var item = new HolidayType();
+        item.Name = type;
+        item.TenantId = "restaff";
+        item.IsActive = true;
+        items.Add(item);
+
+    }
+
+    string json = JsonSerializer.Serialize(items, new JsonSerializerOptions { WriteIndented = true });
+    Console.WriteLine(json);
+}
+
